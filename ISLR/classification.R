8770de63d6a1dc8d21ecf7d89a949e109d99c6b1
rishi <rishi@njorda.se>
2020-10-18 22:10:09 +0200

    islr_strated

---
+++ ISLR/classification.R
@@ -0,0 +1,15 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: rishi
+# Created on: 10/18/2020
+
+
+library(ISLR)
+names(Smarket)
+dim(Smarket)
+summary(Smarket)
+head(Smarket)
+pairs(Smarket)
+
+
+cor(Smarket[,-9])
